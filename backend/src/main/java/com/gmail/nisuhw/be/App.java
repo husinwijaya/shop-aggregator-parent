1b4633a9323e0838d74962c591befba663e2a6e6
Husin Wijaya <wijaya.husin@gmail.com>
2020-07-11 20:37:10 +0700

    init

---
+++ backend/src/main/java/com/gmail/nisuhw/be/App.java
@@ -0,0 +1,13 @@
+package com.gmail.nisuhw.be;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
